20f5ecb29ef93ec120b790a751cb8c73b205d6a6
boniface pereira <bonifacepereira@gmail.com>
2014-10-23 18:50:50 +0530

    somechanges

---
+++ demo.js
@@ -0,0 +1,29 @@
+$(window).resize(function () {
+    adjustWidth();
+});
+function adjustWidth() {
+   $('.sticky-alt').css('width', $('.sticky-alt').parent().width() + 'px');
+}
+$(document).ready(function () {
+    adjustWidth();
+    hljs.initHighlightingOnLoad();
+});
+$(window).scroll(function(){
+    var sti = $('.sticky-alt').parent().offset().top-30
+    console.log(sti);
+    var scr = $(window).scrollTop()
+    console.log(scr);
+
+    if(scr > sti){
+        $('.sticky-alt').css({
+            'position':'fixed',
+            'top':'15px',
+        });
+    }else{
+        $('.sticky-alt').css({
+            'position':'relative',
+            'top':'0px',
+        });
+    }
+
+});
